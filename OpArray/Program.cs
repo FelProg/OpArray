ae2c87fe1d9f27396c8d6cf326e2d8a5020b285e
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-18 11:08:06 -0700

    Agregar archivos de proyecto.

---
+++ OpArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
