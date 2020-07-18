171589510a1ce7936fe712168b689754c5f81c2e
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-18 11:40:15 -0700

    Validando si el arreglo está vacío..

--- OpArray/Program.cs
+++ OpArray/Program.cs
@@ -4,9 +4,33 @@ namespace OpArray
 {
     class Program
     {
+        static string[] Arreglo = new string[10];
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                ValidaVacio();
+                Arreglo[0] = "Cero";
+                ValidaVacio();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private static bool ValidaVacio()
+        {
+            if (Arreglo[0]==null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
     }
 }
