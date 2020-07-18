7c3ccbda8f337a600c74f8602512256dae544534
German Felix Moroyoqui <f.moroyoqui@gmail.com>
2020-07-18 12:39:00 -0700

    validaLleno,existe,agregar

--- OpArray/Program.cs
+++ OpArray/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace OpArray
 {
@@ -9,9 +10,7 @@ namespace OpArray
         {
             try
             {
-                ValidaVacio();
-                Arreglo[0] = "Cero";
-                ValidaVacio();
+                Agregar("Cero");
             }
             catch (Exception ex)
             {
@@ -23,7 +22,60 @@ namespace OpArray
         private static bool ValidaVacio()
         {
             return Arreglo[0] == null ? true : false;
-            
+        }
+
+        private static bool ValidaLleno()
+        {
+            return Arreglo[Arreglo.Length - 1] != null ? true : false;
+        }
+
+        private static bool ValidaExiste(string Dato)
+        {
+            if (!ValidaVacio())//Si el arreglo no esta vacío
+            {
+                foreach(string registro in Arreglo)
+                {
+                    if(registro == null)
+                    {
+                        return false;
+                    }
+
+                    if (registro.ToUpper() == Dato.ToUpper())
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static void Agregar(string Dato)
+        {
+            try
+            {
+                if (ValidaLleno())
+                {
+                    throw new Exception("El arreglo está lleno");
+                }
+
+                if (!ValidaExiste(Dato))
+                {
+                    int arregloLength = Arreglo.Length;
+                    for(int i=0; i<Arreglo.Length; i++)
+                    {
+                        if(Arreglo[i] == null)
+                        {
+                            Arreglo[i] = Dato;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
